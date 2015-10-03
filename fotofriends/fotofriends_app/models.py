517c6356cda14bbca24f253d13d11cffd6ccaeb3
Sebastian <sebastian@Sebastians-MacBook-Pro-6.local>
2015-10-03 04:16:15 -0700

    WIP view for fotofriends_app

--- fotofriends/fotofriends_app/models.py
+++ fotofriends/fotofriends_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Image(models.Model):
-	name = models.CharField(max_length=255)
-	pub_date = models.DateTimeField('date published')
-	image = models.ImageField(upload_to='images')
+    name = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('date published')
+    image = models.ImageField(upload_to='images')
+    def __str__(self):
+        return self.name
